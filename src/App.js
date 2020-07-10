df8e920165170920bfe20db44d255d3182f47c8a
Duván Villa <38322609+elgatoudea@users.noreply.github.com>
2020-07-10 02:17:56 -0500

    Inicio de la instalación basica del proyecto.

--- src/App.js
+++ src/App.js
@@ -8,15 +8,17 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Trabajo de <code>Investigación</code> sobre React.
+       
+          Pagina en contrucción
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="http://duvanvilla.ga/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Perfil del Programador
         </a>
       </header>
     </div>
